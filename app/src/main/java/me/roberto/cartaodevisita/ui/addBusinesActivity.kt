69e401e67e74fd22e04e1b4f0dbea06b3a12d333
Roberto <robertovagner04@gmail.com>
2022-07-21 22:01:36 -0300

    novas telas

---
+++ app/src/main/java/me/roberto/cartaodevisita/ui/addBusinesActivity.kt
@@ -0,0 +1,11 @@
+package me.roberto.cartaodevisita.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addBusinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_busines)
+    }
+}
